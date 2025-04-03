638cb5561b72929304ebb6a5d93c418ffc2391f8
azkriven <azkriven17@gmail.com>
2025-04-03 10:42:16 +0800

    ✨feat: added authjs

---
+++ auth.ts
@@ -0,0 +1,27 @@
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import { DrizzleAdapter } from "@auth/drizzle-adapter";
+import { db } from "@/db";
+import { accounts, sessions, users } from "./db/schema";
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+    adapter: DrizzleAdapter(db, {
+        usersTable: users,
+        accountsTable: accounts,
+        sessionsTable: sessions,
+    }),
+    providers: [
+        GoogleProvider({
+            clientId: process.env.AUTH_GOOGLE_ID!,
+            clientSecret: process.env.AUTH_GOOGLE_SECRET!,
+            authorization: {
+                params: {
+                    prompt: "consent",
+                    access_type: "offline",
+                    response_type: "code",
+                },
+            },
+        }),
+    ],
+    secret: process.env.AUTH_SECRET,
+});
